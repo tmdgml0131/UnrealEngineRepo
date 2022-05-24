fd37801440121f6a195bc8d6c02778e31f07e032
SeungHee Yun ( Scott Yun ) <tmdgml.yun@gmail.com>
2022-05-25 04:06:43 +0900

    Creation_Dungeon
    던전 제작, 광원 제작, 맵 디자인 및
    일부 스터디

---
+++ CryptRaider/Source/CryptRaider/Mover.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Mover.h"
+
+// Sets default values for this component's properties
+UMover::UMover()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UMover::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UMover::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	AActor* Owner = GetOwner();
+	FString Name = Owner->GetActorNameOrLabel();
+	FVector Position = Owner->GetActorLocation();
+	FString OwnerPosition = Position.ToCompactString();
+
+	float MyFloat = 5;
+	float* YourFloat = &MyFloat;
+	UE_LOG(LogTemp, Display, TEXT("Mover Owner : %s"), *OwnerPosition);
+	
+}
+
