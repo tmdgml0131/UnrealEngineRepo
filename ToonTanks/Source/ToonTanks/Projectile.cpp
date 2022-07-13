95d18cc6111b9b50a6109dbb0c4646e463dabb26
SeungHee Yun(윤승희) <tmdgml.yun@gmail.com>
2022-07-13 22:58:07 +0900

    ToonTanks_220713
    Addition of Health Component and damage handling method

--- ToonTanks/Source/ToonTanks/Projectile.cpp
+++ ToonTanks/Source/ToonTanks/Projectile.cpp
@@ -15,8 +15,8 @@ AProjectile::AProjectile()
 	RootComponent = ProjectileMesh;
 
 	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Movement Component"));
-	ProjectileMovementComponent->MaxSpeed = 1300.f;
-	ProjectileMovementComponent->InitialSpeed = 1300.f;
+	ProjectileMovementComponent->MaxSpeed = 3300.f;
+	ProjectileMovementComponent->InitialSpeed = 3300.f;
 }
 
 // Called when the game starts or when spawned
@@ -24,6 +24,7 @@ void AProjectile::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	ProjectileMesh->OnComponentHit.AddDynamic(this, &AProjectile::OnHit);
 }
 
 // Called every frame
@@ -31,5 +32,12 @@ void AProjectile::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void AProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	UE_LOG(LogTemp, Warning, TEXT("HitComp %s"), *HitComp->GetName());
+	UE_LOG(LogTemp, Warning, TEXT("OtherActor %s"), *OtherActor->GetName());
+	UE_LOG(LogTemp, Warning, TEXT("OtherComp %s"), *OtherComp->GetName());
 }
 
