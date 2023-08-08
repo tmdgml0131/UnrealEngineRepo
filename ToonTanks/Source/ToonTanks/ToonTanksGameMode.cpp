53a445ff2e5038019c6dafbba786bc79b4dc1ee5
SeungHee Yun(윤승희) <tmdgml.yun@gmail.com>
2023-08-09 00:56:35 +0900

    ToonTanks
    ToonTanks
    PlayerHandle
    StartTimer

--- ToonTanks/Source/ToonTanks/ToonTanksGameMode.cpp
+++ ToonTanks/Source/ToonTanks/ToonTanksGameMode.cpp
@@ -5,6 +5,7 @@
 #include "Kismet/GameplayStatics.h"
 #include "Tank.h"
 #include "Tower.h"
+#include "ToonTanksPlayerController.h"
 
 void AToonTanksGameMode::ActorDied(AActor* DeadActor)
 {
@@ -12,10 +13,9 @@ void AToonTanksGameMode::ActorDied(AActor* DeadActor)
     {
         Tank->HandleDestruction();
         
-        if(Tank->GetTankPlayerController())
+        if(ToonTanksPlayerController)
         {
-            Tank->DisableInput(Tank->GetTankPlayerController());
-            Tank->GetTankPlayerController()->bShowMouseCursor = false;
+            ToonTanksPlayerController->SetPlayerEnabledState(false);
         }
     }
     else if(ATower* DestroyedTower =  Cast<ATower>(DeadActor))
@@ -28,5 +28,30 @@ void AToonTanksGameMode::BeginPlay()
 {
     Super::BeginPlay();
 
+    HandleGameStart();
+}
+
+void AToonTanksGameMode::HandleGameStart()
+{
     Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+    ToonTanksPlayerController = Cast<AToonTanksPlayerController>(UGameplayStatics::GetPlayerController(this, 0));
+
+    if(ToonTanksPlayerController)
+    {
+        ToonTanksPlayerController->SetPlayerEnabledState(false);
+
+        FTimerHandle PlayerEnableTimerHandle;
+        FTimerDelegate PlayerEnableTImerDelegate = FTimerDelegate::CreateUObject(
+            ToonTanksPlayerController,
+            &AToonTanksPlayerController::SetPlayerEnabledState,
+            true
+        );
+
+        GetWorldTimerManager().SetTimer(
+            PlayerEnableTimerHandle, 
+            PlayerEnableTImerDelegate,
+            StartDelay, 
+            false
+        );
+    }
 }
