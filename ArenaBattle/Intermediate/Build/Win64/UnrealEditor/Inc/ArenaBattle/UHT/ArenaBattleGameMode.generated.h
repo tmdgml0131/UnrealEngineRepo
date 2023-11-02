ed94c393c7fee888f66fa4d7242f3dd67e2de973
SeungHee Yun(윤승희) <tmdgml.yun@gmail.com>
2023-11-02 23:06:51 +0900

    ArenaBattle_Creation
    언리얼 Top-Down view 게임

---
+++ ArenaBattle/Intermediate/Build/Win64/UnrealEditor/Inc/ArenaBattle/UHT/ArenaBattleGameMode.generated.h
@@ -0,0 +1,96 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "ArenaBattleGameMode.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ARENABATTLE_ArenaBattleGameMode_generated_h
+#error "ArenaBattleGameMode.generated.h already included, missing '#pragma once' in ArenaBattleGameMode.h"
+#endif
+#define ARENABATTLE_ArenaBattleGameMode_generated_h
+
+#define FID_Git_UnrealEngineRepo_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_SPARSE_DATA
+#define FID_Git_UnrealEngineRepo_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_RPC_WRAPPERS
+#define FID_Git_UnrealEngineRepo_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_Git_UnrealEngineRepo_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_ACCESSORS
+#define FID_Git_UnrealEngineRepo_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAArenaBattleGameMode(); \
+	friend struct Z_Construct_UClass_AArenaBattleGameMode_Statics; \
+public: \
+	DECLARE_CLASS(AArenaBattleGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/ArenaBattle"), ARENABATTLE_API) \
+	DECLARE_SERIALIZER(AArenaBattleGameMode)
+
+
+#define FID_Git_UnrealEngineRepo_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAArenaBattleGameMode(); \
+	friend struct Z_Construct_UClass_AArenaBattleGameMode_Statics; \
+public: \
+	DECLARE_CLASS(AArenaBattleGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/ArenaBattle"), ARENABATTLE_API) \
+	DECLARE_SERIALIZER(AArenaBattleGameMode)
+
+
+#define FID_Git_UnrealEngineRepo_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	ARENABATTLE_API AArenaBattleGameMode(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AArenaBattleGameMode) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(ARENABATTLE_API, AArenaBattleGameMode); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AArenaBattleGameMode); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	ARENABATTLE_API AArenaBattleGameMode(AArenaBattleGameMode&&); \
+	ARENABATTLE_API AArenaBattleGameMode(const AArenaBattleGameMode&); \
+public: \
+	ARENABATTLE_API virtual ~AArenaBattleGameMode();
+
+
+#define FID_Git_UnrealEngineRepo_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	ARENABATTLE_API AArenaBattleGameMode(AArenaBattleGameMode&&); \
+	ARENABATTLE_API AArenaBattleGameMode(const AArenaBattleGameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(ARENABATTLE_API, AArenaBattleGameMode); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AArenaBattleGameMode); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AArenaBattleGameMode) \
+	ARENABATTLE_API virtual ~AArenaBattleGameMode();
+
+
+#define FID_Git_UnrealEngineRepo_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_9_PROLOG
+#define FID_Git_UnrealEngineRepo_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Git_UnrealEngineRepo_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_SPARSE_DATA \
+	FID_Git_UnrealEngineRepo_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_RPC_WRAPPERS \
+	FID_Git_UnrealEngineRepo_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_ACCESSORS \
+	FID_Git_UnrealEngineRepo_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_INCLASS \
+	FID_Git_UnrealEngineRepo_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_Git_UnrealEngineRepo_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Git_UnrealEngineRepo_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_SPARSE_DATA \
+	FID_Git_UnrealEngineRepo_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Git_UnrealEngineRepo_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_ACCESSORS \
+	FID_Git_UnrealEngineRepo_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_INCLASS_NO_PURE_DECLS \
+	FID_Git_UnrealEngineRepo_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ARENABATTLE_API UClass* StaticClass<class AArenaBattleGameMode>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Git_UnrealEngineRepo_ArenaBattle_Source_ArenaBattle_ArenaBattleGameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
