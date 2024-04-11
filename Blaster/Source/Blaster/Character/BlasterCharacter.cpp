2cdc35202ffe2d2ea8d807ab6ee8d4b8973e8d02
SeungHee Yun ( Scott Yun ) <83335040+tmdgml0131@users.noreply.github.com>
2024-04-11 17:15:05 +0900

    Blaster - Animation
    Reload Animation

--- Blaster/Source/Blaster/Character/BlasterCharacter.cpp
+++ Blaster/Source/Blaster/Character/BlasterCharacter.cpp
@@ -200,16 +200,16 @@ void ABlasterCharacter::PlayReloadMontage()
 			SectionName = FName("RocketLauncher");
 			break;
 		case EWeaponType::EWT_Pistol:
-			SectionName = FName("Rifle");
+			SectionName = FName("Pistol");
 			break;
 		case EWeaponType::EWT_SubmachineGun:
-			SectionName = FName("Rifle");
+			SectionName = FName("Pistol");
 			break;
 		case EWeaponType::EWT_Shotgun:
-			SectionName = FName("Rifle");
+			SectionName = FName("Shotgun");
 			break;
 		case EWeaponType::EWT_SniperRifle:
-			SectionName = FName("Rifle");
+			SectionName = FName("SniperRifle");
 			break;
 		case EWeaponType::EWT_GrenadeLauncher:
 			SectionName = FName("Rifle");
